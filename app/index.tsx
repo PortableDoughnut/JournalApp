dd99a33747991292b1bb6ffceebc55812e91e0a7
Gwen <gwen.thelin@gmail.com>
2025-05-29 22:35:07 -0600

    Added Enty model and started new index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,14 @@
 import { Link } from 'expo-router';
-import { Button, FlatList, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, FlatList, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Entry } from '../models/entry'
 
-const dummyEntries = [
-  { id: '1', title: 'First entry',  body: 'Today was alright' },
-  { id: '2', title: 'Second entry', body: 'Could use a nap honestly' }
-];
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        padding: 20
-      }}
-    >
-      <FlatList
-        data={dummyEntries}
-        keyExtractor={(item) => item.id}
-        renderItem={
-          ({item}) => (
-            <View style={{ marginBottom: 15 }}>
-              <Text style={{ fontWeight: 'bold' }}>
-                {item.title}
-              </Text>
-            </View>
-          )
-        }
-      />
+  const [items, setItems] = useState<Entry[]>([]);
+  
 
-      <Link href="/new" asChild>
-        <Button title="Add New Entry" />
-      </Link>
-    </View>
+  return (
+    
   );
 }
